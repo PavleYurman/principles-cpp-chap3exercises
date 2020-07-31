96eee6aa5812f276f9f3ce9c73196556fc3ffd7b
Yurman <pavel.koprivec@gmail.com>
2020-07-31 18:23:38 +0200

    Add project files.

---
+++ DistanceConversionEx2/DistanceConverterEx2.cpp
@@ -0,0 +1,22 @@
+#include "std_lib_facilities.h"
+
+/*
+Program that converts from miles to kilometres
+*/
+int main()
+{
+	// type safe initialization
+	int miles{ 0 };
+	int kilometres{ 0 };
+	// output what the program does
+	cout << "Program converts miles to kilometres.\n"
+		 << "Input number of miles: ";
+	// process input
+	cin >> miles;
+	// do calculation
+	kilometres = miles * 1609;
+	// do output
+	cout << "That is " << kilometres << " km\n";// continue vid at 23:09
+
+	return 0;
+}
