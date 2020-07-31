94fdff9771a6b6cd3b2816b35f1d099aba1cc240
Yurman <pavel.koprivec@gmail.com>
2020-07-31 18:38:26 +0200

    Km to meters conversion commit

--- DistanceConversionEx2/DistanceConverterEx2.cpp
+++ DistanceConversionEx2/DistanceConverterEx2.cpp
@@ -9,7 +9,7 @@ int main()
 	int miles{ 0 };
 	int kilometres{ 0 };
 	// output what the program does
-	cout << "Program converts miles to kilometres.\n"
+	cout << "Program converts miles to kilometres and kilometers to meters.\n"
 		 << "Input number of miles: ";
 	// process input
 	cin >> miles;
@@ -17,6 +17,11 @@ int main()
 	kilometres = miles * 1609;
 	// do output
 	cout << "That is " << kilometres << " km\n";// continue vid at 23:09
+	// calculate meters by converting km
+	int meters{ 0 };
+	meters = kilometres * 1000;
+	// do output
+	cout << "That is " << meters << " m\n";
 
 	return 0;
 }
