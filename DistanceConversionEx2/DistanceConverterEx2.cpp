2588712cacfd5ffb142b4665a92d616302084bd4
Yurman <pavel.koprivec@gmail.com>
2021-03-02 18:19:19 +0100

    Exercise 2: Program that test legal and ilegal names for variables.

--- DistanceConversionEx2/DistanceConverterEx2.cpp
+++ DistanceConversionEx2/DistanceConverterEx2.cpp
@@ -1,10 +1,30 @@
 #include "std_lib_facilities.h"
 
 /*
-Program that converts from miles to kilometres
+   Program that tests ilegal and legal variables names
 */
 int main()
 {
+	// Ilegal names:
+	//int double = 0; // invalid combination of type specifiers
+	int _varName = 1; // _ characters are reserved for system // compiler doesnt mind abount that
+	int $variable = 3; // $ characters are not recomended but compiler doesnt mind
+	//int 2number = 3; // variable starts with illegal character number 2 // syntax error: identifier expected // compiler says:
+					//syntax error: 'user-defined literal'
+	//int a ab;//for letter a -> syntax error: 'user-defined literal' local variable is not initialized
+	// and for word ab -> syntax warning:expected a ;
+	// Legal names:
+	int a = 2;
+	int b = 3, c = 4, d = 5;
+	string legal_name = "legal_name";
+	string legalName = "legalName";
+	string legalName1 = "legalName1";
+
+
+	return 0;
+}
+
+/* Exercise 2: Program that converts from miles to kilometres and km to meters
 	// type safe initialization
 	int miles{ 0 };
 	int kilometres{ 0 };
@@ -41,7 +61,7 @@ int main()
 		strMilions += "s";
 	}
 	cout << "That is " << milions << strMilions << " " << strTousands << " tousand meters." << endl;
-	
 
-	return 0;
-}
+
+
+*/
