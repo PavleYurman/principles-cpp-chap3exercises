e17f30606f3f95cc12484d6db2c51a94a8f1b06f
Yurman <pavel.koprivec@gmail.com>
2021-03-02 19:41:15 +0100

    Ex.3: Program calculates sum, difference, product and ratio of two integers and reports this and which number is larger.

--- DistanceConversionEx2/DistanceConverterEx2.cpp
+++ DistanceConversionEx2/DistanceConverterEx2.cpp
@@ -1,25 +1,38 @@
 #include "std_lib_facilities.h"
 
 /*
-   Program that tests ilegal and legal variables names
+   Ex.3: Program that inputs two integer numbers, that stores them in two int variables va1 and var2. It determines smaler and larger number.
+   Then calculates: sum, difference, product and ratio and reports them to the user. 
 */
 int main()
 {
-	// Ilegal names:
-	//int double = 0; // invalid combination of type specifiers
-	int _varName = 1; // _ characters are reserved for system // compiler doesnt mind abount that
-	int $variable = 3; // $ characters are not recomended but compiler doesnt mind
-	//int 2number = 3; // variable starts with illegal character number 2 // syntax error: identifier expected // compiler says:
-					//syntax error: 'user-defined literal'
-	//int a ab;//for letter a -> syntax error: 'user-defined literal' local variable is not initialized
-	// and for word ab -> syntax warning:expected a ;
-	// Legal names:
-	int a = 2;
-	int b = 3, c = 4, d = 5;
-	string legal_name = "legal_name";
-	string legalName = "legalName";
-	string legalName1 = "legalName1";
-
+	// Determine smaler and larger number
+	int val1{ 0 }, val2{ 0 };
+	cout << "Input two integer numbers." << endl;
+	cin >> val1 >> val2;
+	string largerVal = to_string(val1);	
+	string ratio = to_string((double)val1 / val2) + " : " + to_string((double)val2 / val2);
+	if (val2 > val1)
+	{
+		largerVal = to_string(val2);
+		cout << largerVal << " is larger value." << endl;
+		ratio = to_string((double)val1 / val1) + " : " + to_string((double)val2 / val1);
+	}
+	else if (val2 == val1)
+	{
+		cout << "Values are equal." << endl;
+		ratio = " 1 : 1 ";
+	}
+	// Determine sum, difference, product and ratio of val1 and val2
+	int sum = val1 + val2;
+	int difference = abs(val1 - val2);
+	int product = val1 * val2;
+	// Output
+	cout << val1 << " + " << val2 << " = " << sum << endl;
+	cout << "| " << val1 << " - " << val2 << " | = " << difference << endl;
+	cout << val1 << " * " << val2 << " = " << product << endl;
+	cout << val1 << " : " << val2 << " = " << ratio << endl;
+	
 
 	return 0;
 }
@@ -62,6 +75,24 @@ int main()
 	}
 	cout << "That is " << milions << strMilions << " " << strTousands << " tousand meters." << endl;
 
+*/
 
+/* Exercise 2: Program that tests ilegal and legal variables names
+	
+	// Ilegal names:
+	//int double = 0; // invalid combination of type specifiers
+	int _varName = 1; // _ characters are reserved for system // compiler doesnt mind abount that
+	int $variable = 3; // $ characters are not recomended but compiler doesnt mind
+	//int 2number = 3; // variable starts with illegal character number 2 // syntax error: identifier expected // compiler says:
+					//syntax error: 'user-defined literal'
+	//int a ab;//for letter a -> syntax error: 'user-defined literal' local variable is not initialized
+	// and for word ab -> syntax warning:expected a ;
+
+	// Legal names:
+	int a = 2;
+	int b = 3, c = 4, d = 5;
+	string legal_name = "legal_name";
+	string legalName = "legalName";
+	string legalName1 = "legalName1";
 
 */
