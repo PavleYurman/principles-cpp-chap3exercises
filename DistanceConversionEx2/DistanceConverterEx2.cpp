171d098f5f26f78cd4a4613495bd0c537543fcff
Yurman <pavel.koprivec@gmail.com>
2021-03-02 17:54:35 +0100

    Miles to km to meters conversion

--- DistanceConversionEx2/DistanceConverterEx2.cpp
+++ DistanceConversionEx2/DistanceConverterEx2.cpp
@@ -8,21 +8,40 @@ int main()
 	// type safe initialization
 	int miles{ 0 };
 	int kilometres{ 0 };
-	// output what the program does
+	//!"! output what the program does
 	cout << "Program converts miles to kilometres and kilometers to meters.\n"
+		 << "Use only integer values.\n"
 		 << "Input number of miles: ";
 	// process input
 	cin >> miles;
 	// do calculation
 	kilometres = miles * 1609;
 	// do output
-	cout << "That is " << kilometres << " km\n";// continue vid at 23:09
+	cout << "That is " << kilometres << " km.\n";
 	// calculate meters by converting km
 	int meters{ 0 };
 	meters = kilometres * 1000;
 	// do output
-	cout << "That is " << meters << " m\n";
-	// testing the new branch
+	// separate milions, tousands, hundreds and tens plus ones
+	int milions = meters / 1000000;
+	int tempTousands = meters % 1000000;
+	int tousands = tempTousands / 1000;
+	string strTousands = to_string(tousands);
+	if (tousands < 100)
+	{
+		strTousands = "0" + strTousands;
+	}
+	if (tousands < 10)
+	{
+		strTousands = "0" + strTousands;
+	}
+	string strMilions = " milion";
+	if (milions >= 2)
+	{
+		strMilions += "s";
+	}
+	cout << "That is " << milions << strMilions << " " << strTousands << " tousand meters." << endl;
+	
 
 	return 0;
 }
